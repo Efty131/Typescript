e01182bdbb8191efc4b5395540ba476d2523e86f
Efty <efty131331@gmail.com>
2024-04-14 09:44:26 +0600

    first typescript program

---
+++ index.js
@@ -0,0 +1,5 @@
+function count(num1, num2) {
+    console.log(num1 + num2);
+}
+count(20, 32);
+// tsc index.ts => index.js => node index.js
---
+++ index.ts
@@ -0,0 +1,7 @@
+function count(num1: number, num2: number){
+    console.log(num1+num2);
+    
+}
+count(20,32);
+
+// tsc index.ts => index.js => node index.js
